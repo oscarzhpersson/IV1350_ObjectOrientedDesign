c251c053b862188c5978a981b9aa047009c99235
Oscar Persson <opers@kth.se>
2022-05-01 02:16:55 +0200

    Adds functionality to the Receipt class

--- IV1350_Sem3/PointOfSale/src/com/opers/iv1350/model/Receipt.java
+++ IV1350_Sem3/PointOfSale/src/com/opers/iv1350/model/Receipt.java
@@ -1,12 +1,46 @@
 // Package declarations.
 package com.opers.iv1350.model;
 
+// Import declarations.
+import com.opers.iv1350.model.Purchase;
+import java.util.ArrayList;
+import com.opers.iv1350.dto.ItemDTO;
+
 public class Receipt
 {
-    
+
+    private Purchase purchaseOfReceipt;
+
+    /**
+     * Constructor for the Receipt object class.
+     */
     public Receipt ()
     {
-        
+        purchaseOfReceipt = null;
     }
 
+    /**
+     * Setter function setting the relevant purchase for this particular receipt object.
+     */
+    public void setPurchase(Purchase purchase)
+    {
+        purchaseOfReceipt = purchase;
+    }
+
+    /**
+     *  Override for the toString() function.
+     *  Prints the values of the Receipt object.
+     */
+    @Override
+    public String toString()
+    {
+        String items = "";
+
+        for (ItemDTO item : purchaseOfReceipt.getItems())
+        {
+            items += item.getItemName() + "\n";
+        }
+
+        return String.format("*** RECEIPT *** \n\n Time of sale: %s \n Items: \n %s \n Total: %f \n Change: %f", purchaseOfReceipt.getTimeOfSale().toString(), items, purchaseOfReceipt.getTotal(), purchaseOfReceipt.getChange());
+    }
 }
