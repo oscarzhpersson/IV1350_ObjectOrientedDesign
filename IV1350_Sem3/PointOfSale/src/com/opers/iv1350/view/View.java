09ab639060fe2f66ce1e15992523a8898ad34e78
Oscar Persson <opers@kth.se>
2022-05-16 13:21:57 +0200

    Ammends the issue where the controller was printing, adds DTO returning relevant data to the View

--- IV1350_Sem3/PointOfSale/src/com/opers/iv1350/view/View.java
+++ IV1350_Sem3/PointOfSale/src/com/opers/iv1350/view/View.java
@@ -26,32 +26,32 @@ public class View
     public void fakeExecution()
     {
         System.out.println("\n\n");
-        System.out.print("Starts the sale");
+        System.out.print("- Starts the sale");
         System.out.println("\n\n");
         controller.startSale();
 
         System.out.println("\n\n");
-        System.out.print("An item has been scanned");
+        System.out.print("→ An item has been scanned");
         System.out.println("\n\n");
-        controller.enterItem("1", 1);
+        System.out.println(controller.enterItem("1", 1).toString());
 
         System.out.println("\n\n");
-        System.out.print("Two of the same items have been scanned");
+        System.out.print("→ Two of the same items have been scanned");
         System.out.println("\n\n");
-        controller.enterItem("1", 2);
+        System.out.println(controller.enterItem("1", 2).toString());
 
         System.out.println("\n\n");
-        System.out.print("A payment has been registered");
+        System.out.print("→ A payment has been registered");
         System.out.println("\n\n");
         controller.registerPayment(100);
 
         System.out.println("\n\n");
-        System.out.print("Receipt is printed");
+        System.out.print("→ Receipt is printed");
         System.out.println("\n\n");
         controller.printReceipt();
 
         System.out.println("\n\n");
-        System.out.print("Ends the sale");
+        System.out.print("→ Ends the sale");
         System.out.println("\n\n");
         controller.endSale();
     }
