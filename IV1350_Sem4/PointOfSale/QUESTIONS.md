059cd378bdcbe3ead8dc2e153bd97790bdd5940a
Oscar Persson <opers@kth.se>
2022-05-16 21:48:26 +0200

    Adds exception class for NoSuchItem

--- IV1350_Sem4/PointOfSale/QUESTIONS.md
+++ IV1350_Sem4/PointOfSale/QUESTIONS.md
@@ -13,4 +13,7 @@
 
 # Notes during progress:
 
-   
+    Exception --> Checked --> Business logic
+    RuntimeException --> UnChecked --> Bug
+
+   NoSuchItemException handles business logic (entered item not found). Thus it is a checked exception and should be a Exception (Not runtime).
---
+++ IV1350_Sem4/PointOfSale/src/com/opers/iv1350/integration/NoSuchItemException.java
@@ -0,0 +1,31 @@
+package com.opers.iv1350.integration;
+
+/**
+ * Exception to be called when an identifier which does not exist within the catalog is passed.
+ */
+public class NoSuchItemException extends RuntimeException
+{
+
+    private String identifier;
+    
+    /**
+     * Creates a new instance of the exception thrown when an identifier not corresponding to
+     * an existing item is passed.
+     * @param identifier The identifier which was passed, not corresponding to an item in the catalog.
+     */
+    public NoSuchItemException (String identifier)
+    {
+        super(String.format("The queried item identifier, %s, does not correspond to an existing item.", identifier));
+        this.identifier = identifier;
+    }
+
+    /**
+     * Getter function for the identifier which raised the exception.
+     * @return The identifier which did not correspond to an item in the catalog.
+     */
+    public String getNonExistentItemIdentifier()
+    {
+        return identifier;
+    }
+
+}
